68df8ac5c796ccf0eba5865f81a187ebca40cb85
towercity <matthewjnerger3@gmail.com>
2019-08-01 14:10:14 -0400

    Add Cardside logic into App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CardSide from './views/CardSide';
 
 function App() {
+  let word = "word";
+
   return (
     <div>
-      <CardSide />
+      <CardSide word={word} />
       <ResultSide />
     </div>
   )
