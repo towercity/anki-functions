292cc6ab52f8c4a8e5a7b6b15cfb72717f4644b1
towercity <matthewjnerger3@gmail.com>
2019-08-01 22:02:35 -0400

    fix variable names

--- src/data/deck_ids.js
+++ src/data/deck_ids.js
@@ -1,7 +1,7 @@
 const DECK_IDS = {
     subs: 1558903427484,
-    Listening: 1552057366204,​​
-    Other: 1552057382923
+    listening: 1552057366204, 
+    other: 1552057382923
 }
 
 export default DECK_IDS;
