0f4ff25c854b8ff6150cb082bf89764f507db894
towercity <matthewjnerger3@gmail.com>
2019-08-10 14:58:19 -0400

    Gather all information for new cards

--- src/scripts/changeSubs.js
+++ src/scripts/changeSubs.js
@@ -3,6 +3,8 @@
 // proper decks (listening or not) thru an external script and deletes the original subs2srs cards
 
 import Anki from '../services/Anki';
+import DECK_IDS from '../data/deck_ids';
+import MODELS from '../data/models';
 
 // Dummy wrapper around console.log
 // In the future, this function will both console.log and post to a console on the DOM
@@ -18,27 +20,59 @@ const changeSubs = () => {
     logResult('gathering notes...');
     Anki
         // searches Anki for all new cards tagged 00change
-        .findNotes('tag:00change is:new')
+        .findNotes('tag:01change is:new')
         // save the found notes to nodeIds array
         .then(res => {
             noteIds = res;
+            if(noteIds.length === 0) {
+                logResult('no notes found', 'ending function', '');
+                return;
+            }
             logResult(`${noteIds.length} notes gathered`, 'pulling notes information...');
 
-            // searches Anki for the notes info
+            // searches Anki for the notes info then adds new notes with the proper values
             Anki
             .notesInfo(noteIds)
             .then(res => {
-                res.map((note, idx) => {
+                logResult('information pulled', 'creating new notes...')
+                const newNotes = res.map((note, idx) => {
+                    // Create a new array for tags w/o the 00change/01change tag
+                    const newTags = note.tags.filter(tag => tag !== '00change' && tag!== '01change');
+
+                    const newNote = {
+                        "deckname": DECK_IDS.subs,
+                        "modelName": MODELS.japanese,
+                        "fields": {
+                            "Vocabulary": note.fields.Vocab.value,
+                            "Vocabulary-Reading": note.fields['Vocab-Reading'].value,
+                            "Meaning": note.fields['Vocab-Meaning'].value,
+                            "Sentence-1": note.fields.Sentence.value,
+                            "Sentence-1-Reading": note.fields["Sentence-Reading"].value,
+                            "Sentence-1-English": note.fields.English.value,
+                            "Sentence-1-Audio": note.fields.Audio.value,
+                            "Sentence-1-Image": note.fields.Image.value
+                        },
+                        tags: newTags
+                    }
 
                     logResult(
                         '...',
-                        `card ${idx + 1}`,
-                        `sentence: ${note.fields.Sentence.value}`,
-                        `english: ${note.fields.English.value}`,
-                        `notes: ${note.fields.Notes.value}`,
-                        `tags: ${note.tags.join(', ')}`
+                        `note ${idx + 1}`,
+                        `term: ${newNote.fields.Vocabulary}`,
+                        `meaning: ${newNote.fields.Meaning}`,
+                        `sentence: ${newNote.fields['Sentence-1']}`,
+                        `english: ${newNote.fields['Sentence-1-English']}`,
+                        `tags: ${newNote.tags.join(', ')}`
                     )
+
+                    return newNote;
                 })
+
+                logResult(
+                    '...',
+                    `created ${newNotes.length} new notes`,
+                    'sending to Anki...'
+                )
             })
         })
         
