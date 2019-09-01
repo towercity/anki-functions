3e6d48e2c1df311423aed946ad4be1c4c9d5961c
towercity <matthewjnerger3@gmail.com>
2019-09-01 14:19:04 -0400

    Updates

---
+++ precache-manifest.baad9b1684afff8e453bf2d3bc9ea71d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66f59ff00c1066d976cacd1dd6a7450",
+    "url": "/anki-functions/index.html"
+  },
+  {
+    "revision": "aca97b1dcffd233aca39",
+    "url": "/anki-functions/static/css/main.46b3997b.chunk.css"
+  },
+  {
+    "revision": "18f9e724fa468fa1eb1c",
+    "url": "/anki-functions/static/js/2.03f08814.chunk.js"
+  },
+  {
+    "revision": "aca97b1dcffd233aca39",
+    "url": "/anki-functions/static/js/main.53d82b02.chunk.js"
+  },
+  {
+    "revision": "be21d9e25df51411626c",
+    "url": "/anki-functions/static/js/runtime~main.4cfe51b4.js"
+  }
+]);
